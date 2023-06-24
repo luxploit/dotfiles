911201e97c109e44591a2de2d90405ac1b9bf204
Lu Raine <lu.raine@outlook.jp>
2023-06-24 18:17:17 +0200

    dwm and urxvt

--- autorun/autorun.sh
+++ autorun/autorun.sh
@@ -4,8 +4,11 @@ function run {
     pgrep -u $USER -fx "$1" > /dev/null || ($1)
 }
 
-run "sc"
+run "lxpolkit"
 run "picom -b"
+run "setxkbmap de"
+run "xsetroot -cursor_name left_ptr"
+run "xrdb -merge /home/raine/.Xresources"
+run "urxvtd"
 run "nitrogen --restore"
 run "nm-applet"
-run "lxpolkit"
